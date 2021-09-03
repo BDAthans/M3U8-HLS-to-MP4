f8d2596d0be9fdee6b6935d5fd990ab91b14d94c
0xShellcode <BDAthans@yahoo.com>
2021-09-03 05:24:36 -0500

    Add files via upload

---
+++ M3U8 HLS to MP4.cmd
@@ -0,0 +1,20 @@
+@echo off
+Title Download MP4 from M3U8 URL
+setlocal EnableDelayedExpansion
+
+echo ###################################################################################################
+echo #                                                                                                 #
+echo #                           - - Download from M3U8 URL to MP4 - -                                 #
+echo #                                                                                                 #
+echo ###################################################################################################
+echo.
+set /p URL= Paste M3U8 URL here: 
+echo.
+
+if not exist downloaded-mp4s mkdir downloaded-mp4s
+..\ffmpeg\bin\ffmpeg -i "%URL%" -c copy -bsf:a aac_adtstoasc downloaded-mp4s\Output.mp3
+
+echo.
+echo Press Any Key to Exit
+pause >NUL
+explorer %cd%\downloaded-mp4s
